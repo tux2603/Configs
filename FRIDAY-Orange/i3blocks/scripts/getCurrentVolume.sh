b349bfece126b0029187edde2a378c68462f3da8
Ryan Slater <ryan.j.slater.2@gmail.com>
2019-09-12 12:57:22 -0400

    I can't remember what i changed
    definitely added an htop keybinding though

--- FRIDAY-Orange/i3blocks/scripts/getCurrentVolume.sh
+++ FRIDAY-Orange/i3blocks/scripts/getCurrentVolume.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-amixer -D pulse get Master > /home/rjslater/.config/i3blocks/scripts/.currentVolume
+pacmd list-sinks | grep % > /home/rjslater/.config/i3blocks/scripts/.currentVolume
 pacmd list-sinks | grep mute >> /home/rjslater/.config/i3blocks/scripts/.currentVolume
--- FRIDAY-Orange/i3blocks/scripts/volume.py
+++ FRIDAY-Orange/i3blocks/scripts/volume.py
@@ -4,9 +4,9 @@ import subprocess
 subprocess.call('/home/rjslater/.config/i3blocks/scripts/getCurrentVolume.sh')
 volumeData = [i for i in open('/home/rjslater/.config/i3blocks/scripts/.currentVolume', 'r')]
 
-volume = int(float(volumeData[5][volumeData[5].find('[') + 1:volumeData[5].find(']') - 1].strip()))
+volume = int(float(volumeData[0][volumeData[0].find('/') + 1:volumeData[0].find('%')].strip()))
 muted = False
-for i in range(4, 7):
+for i in range(len(volumeData)):
     if 'yes' in volumeData[i]:
         muted = True
         break
