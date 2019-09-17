e15746946b5c4d85db4388737115f9f632f728c0
Ryan Slater <ryan.j.slater.2@gmail.com>
2019-09-17 14:13:02 -0400

    Delete toggleWifi.sh
    unused

--- FRIDAY-Orange/i3/toggleWifi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if nmcli radio wifi | grep -q "disabled" ; then
-    nmcli radio wifi on
-    notify-send -t 2000 -i network-wireless-full "Wireless enabled" "Your wireless adaptor has been enabled."
-else
-    nmcli radio wifi off
-    notify-send -t 2000 -i network-wireless-disconnected "Wireless disabled" "Your wireless adaptor has been disabled."
-fi
